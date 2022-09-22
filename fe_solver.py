c5e6a212a758ea5037280852e4139f575a4a4e4c
Jonathan Lin <jon880@astro.ucla.edu>
2022-09-21 18:30:00 -0700

    updated mesher to make PL meshes

--- fe_solver.py
+++ fe_solver.py
@@ -3,15 +3,8 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import pygmsh
 from scipy.linalg import eigh
-
-def circ_points(radius,res):
-    thetas = np.linspace(0,2*np.pi,res,endpoint=False)
-    points = []
-    for t in thetas:
-        points.append((radius*np.cos(t),radius*np.sin(t)))
-    return points
+from mesher import plot_mesh,lantern_mesh,circ_points
 
 def IOR_fiber(r,n,n0):
     def inner(x,y):
@@ -20,141 +13,77 @@ def IOR_fiber(r,n,n0):
         return n0
     return inner
 
-def construct_mesh(w,r,res):
-    ''' construct triangular mesh for FE analysis. a mesh is a tuple of points and connections.
-        points is a 2D numpy array of all nodes in the mesh (whose positions are length 2 numpy arrays)
-        each element of connections is a list containing the 6 indices that locate the nodes in each tri
-
-        w: width of comp zone (assumed square)
-        r: core radius (need to make this more extensible beyond circular step-index later)
-    '''
-    with pygmsh.occ.Geometry() as geom:
-        rect = geom.add_rectangle((-w/2,-w/2,0),w,w) 
-        circ = geom.add_disk((0,0),r,mesh_size=w/res)
-        geom.boolean_difference(rect,circ,delete_other = False)
-        mesh = geom.generate_mesh(dim=2,order=2)
-        points = mesh.points
-        tris = mesh.cells[1].data
-        return (points,tris)
-
-def construct_mesh_circ(w,r,res):
-    ''' construct triangular mesh for FE analysis. a mesh is a tuple of points and connections. outer bound 
-        is a circle. points is a 2D numpy array of all nodes in the mesh (whose positions are length 2 numpy arrays)
-        each element of connections is a list containing the 6 indices that locate the nodes in each tri
-
-        w: width of comp zone (assumed square)
-        r: core radius (need to make this more extensible beyond circular step-index later)
-    '''
-    with pygmsh.occ.Geometry() as geom:
-        circ0 = geom.add_polygon(circ_points(w/2,int(res/2)))
-        circ = geom.add_disk((0,0),r,mesh_size=w/res)
-        geom.boolean_difference(circ0,circ,delete_other = False)
-        mesh = geom.generate_mesh(dim=2,order=2)
-        points = mesh.points
-        tris = mesh.cells[1].data
-        return (points,tris)
-
-def plot_mesh(mesh,IOR_arr = None,show=True):
-    points,tris = mesh
-
-    if show:
-        fig,ax = plt.subplots(figsize=(5,5))
-
-    for point in points:
-        plt.plot(point[0],point[1],'ko',ms=2)
-
-    cm = plt.get_cmap("viridis")
-    for i,_e in enumerate(tris):
-        e = _e[:3]
-        
-        if IOR_arr is not None:
-            n,n0 = np.max(IOR_arr),np.min(IOR_arr)
-            cval = IOR_arr[i]/(n-n0) - n0/(n-n0)
-            color = cm(cval)
-            t=plt.Polygon(points[e][:,:2], ec='k', fc=color,lw=0.7)
-        else:
-            t=plt.Polygon(points[e][:,:2], ec='k', fc='None',lw=0.7)
-        plt.gca().add_patch(t)
-
-    plt.xlim(np.min(points[:,0]),np.max(points[:,0]) )
-    plt.ylim(np.min(points[:,1]),np.max(points[:,1]) )
-    if show:
-        plt.show()
-
-def discretize_IOR(IOR_func,mesh):
-    ''' discretize an input function IOR(x,y) on the faces of a mesh'''
-    out = []
-    points,tris = mesh
-    for tri in tris:
-        c = np.mean(points[tri[:3]],axis=0)
-        out.append(IOR_func(c[0],c[1]))
-    return np.array(out)
-
-def construct_AB(mesh,IOR_arr,k,IOR_func = None):
+def construct_AB(mesh,IOR_dict,k):
     from shape_funcs import compute_dNdN, compute_NN
-    points,qtris = mesh
+    
+    points = mesh.points
+    materials = mesh.cell_sets.keys()
 
     N = len(points)
 
     A = np.zeros((N,N))
     B = np.zeros((N,N))
 
-    for tri,IOR in zip(qtris,IOR_arr):
-        tri_points = points[tri]
-        NN = compute_NN(tri_points)
-        dNdN = compute_dNdN(tri_points)
-        #NN,dNdN = compute_matrices_from_file(tri_points)
-        
-        ix = np.ix_(tri,tri)
-        
-        if IOR_func is not None:
-            IORs_alt = np.array([IOR_func(*points[i][:2]) for i in tri])
-            A[ix] += k**2*NN * np.power(IORs_alt[None,:],2) - dNdN
-        else:
-            A[ix] += k**2*IOR**2 * NN - dNdN
-        
-        B[ix] += NN
-    
+    for material in materials:
+        tris = mesh.cells[1].data[tuple(mesh.cell_sets[material])][0,:,0,:]
+        for tri in tris:
+            tri_points = points[tri]
+            NN = compute_NN(tri_points)
+            dNdN = compute_dNdN(tri_points)
+            ix = np.ix_(tri,tri)
+            A[ix] += k**2*IOR_dict[material]**2 * NN - dNdN
+            B[ix] += NN
+
     return A,B
 
-def solve(A,B,mesh,k,ncore,nclad,plot=False):
+def solve(A,B,mesh,k,IOR_dict,plot=False):
     w,v = eigh(A,B)
-    points,tris = mesh
+
+    IORs = [ior[1] for ior in IOR_dict.items()]
+    nmin,nmax = min(IORs) , max(IORs)
+
     if plot:
         for _w,_v in zip(w[::-1],v.T[::-1]):
             ne = np.sqrt(_w/k**2)
-            if not (nclad <= ne <= ncore):
+            if not (nmin <= ne <= nmax):
                 print("warning: spurious mode! ")
             
             print("effective index: ",np.sqrt(_w/k**2))
             plot_eigenvector(mesh,_v)
 
-def plot_eigenvector(mesh,v):
-    plt.figure(figsize=(8,8))
+def plot_eigenvector(mesh,v,plot_mesh = False):
+    points = mesh.points
+    fig,ax = plt.subplots(figsize=(5,5))
     plt.axis('equal')
     plt.tricontourf(points[:,0],points[:,1],v,levels=40)
     plt.colorbar()
-    plot_mesh(mesh,show=False)
+    if plot_mesh:
+        plot_mesh(mesh,show=False,ax=ax)
+    try:
+        circle = plt.Circle((0,0),mesh.cell_data['radius'],ec='white',fc='None',lw=2)
+        ax.add_patch(circle)
+    except:
+        pass
     plt.show()
 
-ncore = 1.444
-nclad = 1.444-5.5e-3
+ncore = 1.444 + 0.01036
+nclad = 1.444
+njack = 1.444 - 5.5e-3
+
+IOR_dict = {"core":ncore,"cladding":nclad,"jacket":njack}
+
 k = 2*np.pi/1.55
 res = 40
 
 r = 10
 w = 4*r
-IOR_func = IOR_fiber(r,ncore,nclad)
 
-mesh = construct_mesh_circ(w,r,res)
-
-points,tris = mesh
-
-IOR_arr = discretize_IOR(IOR_func,mesh)
+cores = [(0,0)] + circ_points(2*r/3,5)
+mesh = lantern_mesh(w/2,r,cores,r/10,res)
 
 print("mesh and refractive index distribution")
-plot_mesh(mesh,IOR_arr=IOR_arr)
+plot_mesh(mesh,IOR_dict=IOR_dict)
 
-A,B = construct_AB(mesh,IOR_arr,k)
+A,B = construct_AB(mesh,IOR_dict,k)
 
-solve(A,B,mesh,k,ncore,nclad,plot=True)
+solve(A,B,mesh,k,IOR_dict,plot=True)
--- mesher.py
+++ mesher.py
@@ -1,35 +1,43 @@
 import pygmsh
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
 
-def circ_points(radius,res):
+def circ_points(radius,res,center=(0,0)):
     thetas = np.linspace(0,2*np.pi,res,endpoint=False)
     points = []
     for t in thetas:
-        points.append((radius*np.cos(t),radius*np.sin(t)))
+        points.append((radius*np.cos(t)+center[0],radius*np.sin(t)+center[1]))
     return points
 
-def plot_mesh(mesh,IOR_arr = None,show=True):
-    points,tris = mesh
+def plot_mesh(mesh,IOR_dict=None,show=True,ax=None):
+    points = mesh.points
+    tris = mesh.cells[1].data
+    materials = mesh.cell_sets.keys()
 
-    if show:
+    if IOR_dict is not None:
+        IORs = [ior[1] for ior in IOR_dict.items()]
+        n,n0 = max(IORs) , min(IORs)
+
+    if show and ax is None:
         fig,ax = plt.subplots(figsize=(5,5))
+
+    for material in materials:
+        if IOR_dict is not None:        
+            cval = IOR_dict[material]/(n-n0) - n0/(n-n0)
+            cm = plt.get_cmap("viridis")
+            color = cm(cval)
+        else:
+            color = "None"
+        _tris = tris[tuple(mesh.cell_sets[material])][0,:,0,:]
+        patches = []
+        for i,_tri in enumerate(_tris):
+            t=plt.Polygon(points[_tri[:3]][:,:2], ec='k', facecolor=color,lw=0.7)
+            patches.append(t)
+            ax.add_patch(t)
 
     for point in points:
         plt.plot(point[0],point[1],'ko',ms=2)
-
-    cm = plt.get_cmap("viridis")
-    for i,_e in enumerate(tris):
-        e = _e[:3]
-        
-        if IOR_arr is not None:
-            n,n0 = np.max(IOR_arr),np.min(IOR_arr)
-            cval = IOR_arr[i]/(n-n0) - n0/(n-n0)
-            color = cm(cval)
-            t=plt.Polygon(points[e][:,:2], ec='k', fc=color,lw=0.7)
-        else:
-            t=plt.Polygon(points[e][:,:2], ec='k', fc='None',lw=0.7)
-        plt.gca().add_patch(t)
 
     plt.xlim(np.min(points[:,0]),np.max(points[:,0]) )
     plt.ylim(np.min(points[:,1]),np.max(points[:,1]) )
@@ -66,9 +74,12 @@ def construct_mesh2(w,r,res,ret="tuple"):
     '''
     with pygmsh.occ.Geometry() as geom:
         circ0 = geom.add_polygon(circ_points(w/2,int(res/2)))
-        circ = geom.add_disk((0,0),r,mesh_size=w/res)
+        circ = geom.add_polygon(circ_points(r,res))
 
-        geom.boolean_difference(circ0,circ,delete_other = False)
+        #cladding = geom.boolean_difference(circ0,circ,)
+
+        union = geom.boolean_union([circ0,circ])
+        geom.boolean_fragments(union,circ)
         mesh = geom.generate_mesh(dim=2,order=2,algorithm=6)
         points = mesh.points
         tris = mesh.cells[1].data
@@ -77,5 +88,42 @@ def construct_mesh2(w,r,res,ret="tuple"):
         else:
             return mesh
 
-m = construct_mesh2(2,0.5,40)
-plot_mesh(m)
+def lantern_mesh(r_jack,r_clad,pos_core,r_core,res):
+    """ construct a mesh that conforms to a 'lantern' structure: circular jacker,
+    smaller circular cladding, and even smaller circular inclusions (cores) at
+    arbitrary locations """
+
+    with pygmsh.occ.Geometry() as geom:
+        jacket_base = geom.add_polygon(circ_points(r_jack,int(res/2)))
+        cladding_base = geom.add_polygon(circ_points(r_clad,res))
+
+        jacket = geom.boolean_difference(jacket_base,cladding_base,delete_other = False)
+        geom.add_physical(jacket,"jacket")
+
+        if type(r_core) != list and type(pos_core) == list:
+            rs = [r_core] * len(pos_core)
+            ps = pos_core
+        elif type(r_core) != list and type(pos_core) != list:
+            rs = [r_core]
+            ps = [pos_core]
+
+        cores = []
+        for r,p in zip(rs,ps):
+            core = geom.add_disk(p,r,mesh_size = r)
+            cores.append(core)
+
+        geom.add_physical(cores,"core")
+        cladding = geom.boolean_difference(cladding_base,cores,delete_other=False)
+        geom.add_physical(cladding,"cladding")
+        mesh = geom.generate_mesh(dim=2,order=2,algorithm=6)
+        mesh.cell_data["radius"] = r_clad
+        return mesh
+
+if __name__ == "__main__":
+    cores = [(0,0)] + circ_points(0.25,5)
+    m = lantern_mesh(1,0.5,cores,0.05,30)
+    #m = construct_mesh2(2,0.5,30)
+
+    IOR_dict = {"jacket":2,"cladding":1.5,"core":1}
+    
+    plot_mesh(m,IOR_dict)

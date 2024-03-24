16d54d15136c901007f5ac1b0637f67c5a97d32c
Jonathan Lin <90280749+jw-lin@users.noreply.github.com>
2024-03-24 13:29:37 -0700

    better julia setup

--- src/wavesolve/FEvalsetup.py
+++ src/wavesolve/FEvalsetup.py
@@ -2,6 +2,11 @@ from juliacall import Pkg as jlPkg
 import wavesolve,os
 path = os.path.dirname(wavesolve.__file__)
 
-jlPkg.activate(path+"/FEval")
-jlPkg.add("PythonCall")
-jlPkg.add("StaticArrays")
+
+def FEvalsetup():
+    path = os.path.dirname(wavesolve.__file__)
+    #jlPkg.develop(jlPkg.PackageSpec(path = path+"\FEval") )
+    jlPkg.activate(path+"/FEval")
+    jlPkg.add("StaticArrays")
+    jlPkg.add("PythonCall")
+    jlPkg.precompile()

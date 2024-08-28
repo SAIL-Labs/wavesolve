8b3c6a95f6828669c32432910ebe358babc9018b
aditya-sengupta <adityars@ucsc.edu>
2024-08-27 21:10:33 -0700

    speedups being rolled back

--- src/wavesolve/shape_funcs.py
+++ src/wavesolve/shape_funcs.py
@@ -189,6 +189,59 @@ def compute_dNdN(tri,_J=None,row=None):
 
     return out
 
+def compute_NN_dNdN_vec(tris,row=None):
+    NN_out = np.zeros((len(tris),6,6),dtype=np.float64)
+    
+    # nodes assumed to be ordered vertices (clockwise) then edges (clockwise). first vertex is origin of affine transform
+    x21 = tris[:,1,0] - tris[:,0,0]
+    y21 = tris[:,1,1] - tris[:,0,1]
+    x31 = tris[:,2,0] - tris[:,0,0]
+    y31 = tris[:,2,1] - tris[:,0,1]
+    x32 = tris[:,2,0] - tris[:,1,0]
+    y32 = tris[:,2,1] - tris[:,1,1]
+    _J = x21*y31 - x31*y21
+
+    ## diagonals
+    NN_out[:,0,0] = NN_out[:,1,1] = NN_out[:,2,2] = _J/60
+    NN_out[:,3,3] = NN_out[:,4,4] = NN_out[:,5,5] = 4*_J/45
+    ## off diagonals
+    # vertex only relations
+    NN_out[:,1,0] = NN_out[:,0,1] = NN_out[:,1,2] = NN_out[:,2,1] = NN_out[:,2,0] = NN_out[:,0,2] = -_J/360
+    # vertex - adjacent edge relations: 0
+    # vertex - opposite edge relations
+    NN_out[:,0,4] = NN_out[:,4,0] = NN_out[:,1,5] = NN_out[:,5,1] = NN_out[:,2,3] = NN_out[:,3,2] = -_J/90
+    # edge edge relations
+    NN_out[:,3,4] = NN_out[:,4,3] = NN_out[:,3,5] = NN_out[:,5,3] = NN_out[:,4,5] = NN_out[:,5,4] = 2*_J/45
+
+    out = np.zeros((len(tris),6,6),dtype=np.float64)
+
+    # diagonals
+    out[:,0,0] = (y32*y32 + x32*x32)/(2*_J)
+    out[:,1,1] = (y31*y31 + x31*x31)/(2*_J)
+    out[:,2,2] = (y21*y21 + x21*x21)/(2*_J)
+    out[:,3,3] = 4/(3*_J) * (y32**2+y31*y21+x32**2+x31*x21)
+    out[:,4,4] = 4/(3*_J) * (y31**2-y21*y32+x31**2-x21*x32)
+    out[:,5,5] = 4/(3*_J) * (y21**2+y32*y31+x21**2+x32*x31)
+
+    # vertex - vertex (going clockwise)
+    out[:,0,1] = out[:,1,0] = (y32*y31+x32*x31)/(6*_J)
+    out[:,1,2] = out[:,2,1] = (y31*y21+x31*x21)/(6*_J)
+    out[:,2,0] = out[:,0,2] = (-y21*y32-x21*x32)/(6*_J)
+
+    # vertex - adjacent edge
+    out[:,0,3] = out[:,3,0] = out[:,1,3] = out[:,3,1]  = -2*(y32*y31+x32*x31)/(3*_J)
+    out[:,0,5] = out[:,5,0] = out[:,2,5] = out[:,5,2] = 2*(y21*y32+x21*x32)/(3*_J)
+    out[:,2,4] = out[:,4,2] = out[:,1,4] = out[:,4,1] = -2*(y31*y21+x31*x21)/(3*_J)
+
+    # vertex - opposite edge: 0
+
+    # edge-edge
+    out[:,3,4] = out[:,4,3] =  4/(3*_J)*(y21*y32+x21*x32)
+    out[:,4,5] = out[:,5,4] = -4/(3*_J)*(y31*y32+x31*x32)
+    out[:,5,3] = out[:,3,5] = -4/(3*_J)*(y31*y21+x31*x21)
+
+    return NN_out, out
+
 def compute_dNdN_precomp(row):
     out = np.zeros((6,6),dtype=np.float64)
 
